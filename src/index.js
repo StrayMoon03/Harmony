fd9e1afbb590c74d9dfd3e745e47ce11c7fc77f7
StrayMoon03 <kristina.moon.us@gmail.com>
2026-08-03 01:28:44 -0400

    Initial Harmony bot setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("clientReady", () => {
+console.log(`💜 Harmony is online as ${client.user.username}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
